78c8b6889f4ed8cf984f3f02130c66bc87178069
Mikhail Guschin <mguschin@gmail.com>
2021-07-26 20:18:36 +0300

    Removed unnecessary interfaces.

--- src/main/java/ru/mguschin/phonenumberchecker/service/PhoneDao.java
+++ src/main/java/ru/mguschin/phonenumberchecker/service/PhoneDao.java
@@ -1,6 +1,39 @@
 package ru.mguschin.phonenumberchecker.service;
 
-public interface PhoneDao {
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.jdbc.core.namedparam.MapSqlParameterSource;
+import org.springframework.jdbc.core.namedparam.SqlParameterSource;
+import org.springframework.jdbc.core.namedparam.NamedParameterJdbcTemplate;
+import org.springframework.jdbc.core.RowMapper;
+import org.springframework.stereotype.Repository;
 
-    String phoneCheck(String phone, String requestId);
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import javax.sql.DataSource;
+
+@Repository
+public class PhoneDao {
+
+    private final NamedParameterJdbcTemplate jdbcTemplate;
+
+    @Autowired
+    public PhoneDao (DataSource dataSource) {
+        jdbcTemplate = new NamedParameterJdbcTemplate(dataSource);
+    }
+
+    public String phoneCheck(String phone, String requestId) {
+
+        SqlParameterSource namedParameters = new MapSqlParameterSource().addValue("p1", phone).addValue("p2", phone.substring(1));
+
+        String query = "select count(*) as RESULT from ((select 1 as RESULT from TABLE1 where PHONE = :p1 limit 1) union all (select 1 from TABLE2 where PHONE = :p2 limit 1)) as t";
+
+        Integer res = jdbcTemplate.queryForObject(query, namedParameters, Integer.class);
+
+        switch (res.intValue()) {
+            case 0: return "ACCEPT";
+            case 1: return "CHALLENGE";
+            default: return "DECLINE";
+        }
+    }
 }
--- src/main/java/ru/mguschin/phonenumberchecker/service/PhoneService.java
+++ src/main/java/ru/mguschin/phonenumberchecker/service/PhoneService.java
@@ -1,6 +1,18 @@
 package ru.mguschin.phonenumberchecker.service;
 
-public interface PhoneService {
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+import javax.annotation.Resource;
 
-    String check(String phone, String requestId);
+@Service
+@Transactional
+public class PhoneService {
+
+    @Resource
+    private PhoneDao phoneDao;
+
+    public String check(String phone, String requestId) {
+
+        return phoneDao.phoneCheck(phone, requestId);
+    }
 }
